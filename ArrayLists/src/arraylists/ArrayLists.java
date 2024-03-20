b783ea5d8adc88c5b152693e3f6618fce88f4ea6
juanjoseguerrer <jjguerreroa@ufpso.edu.co>
2024-03-20 15:11:37 -0500

    Arraylist

---
+++ ArrayLists/src/arraylists/ArrayLists.java
@@ -0,0 +1,30 @@
+
+package arraylists;
+import java.util.ArrayList;
+
+public class ArrayLists {
+
+    
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        
+        System.out.println(arrayList.size());
+        System.out.println(arrayList.isEmpty());
+        
+        arrayList.add("hola");
+        arrayList.add("mundo");
+        arrayList.add("cruel");
+        ArrayList<String> total = new ArrayList<>();
+        total.add("nuevo");
+        total.addAll(arrayList);
+        System.out.println(total);
+        
+        arrayList.set(2, "bonito");
+        arrayList.remove(2);
+        
+        System.out.println(arrayList);
+        System.out.println(arrayList.get(1));
+        
+    }
+    
+}
